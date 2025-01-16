7cd8de8c55a74f3e370efad90418368cfec83958
Jeric Epon <jericepon@gmail.com>
2025-01-16 06:49:25 +0000

    Refactor ProductCard component for improved layout and styling; add stock information and enhance loader design

--- src/components/shared/ProductCard.tsx
+++ src/components/shared/ProductCard.tsx
@@ -10,43 +10,49 @@ import {
 import { AspectRatio } from '../ui/aspect-ratio'
 import { Product } from "@/models/Product"
 import { Skeleton } from "../ui/skeleton"
+import { Plus } from "lucide-react"
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Card className="flex flex-col overflow-hidden">
-      <AspectRatio ratio={4 / 3} className="overflow-hidden">
-        <img src={product.thumbnail} alt={product.title} className="w-full" />
-      </AspectRatio>
-      <CardHeader>
-        <CardTitle>{product.title}</CardTitle>
-        <CardDescription>{product.description}</CardDescription>
-      </CardHeader>
-      <div className="px-6 pt-0">
-        <p>${product.price}</p>
-      </div>
-      <CardFooter className="flex justify-between mt-auto">
-        <Button className="ml-auto">Add to cart</Button>
-      </CardFooter>
-    </Card>
+    <>
+      <Card className="flex flex-col">
+        <AspectRatio ratio={1 / 1} className="overflow-hidden p-6">
+          <img src={product.thumbnail} alt={product.title} className="w-full bg-muted rounded-xl" />
+        </AspectRatio>
+        <CardHeader className="pt-0 mt-auto">
+          <div className="grid grid-cols-3 gap-2">
+            <div className="flex flex-col space-y-2 col-span-2">
+              <CardTitle>{product.title}</CardTitle>
+              <div className="text-sm text-muted-foreground col-span-2">{product.brand}</div>
+            </div>
+            <Button variant="outline" size="icon" className="my-auto ml-auto">
+              <Plus />
+            </Button>
+          </div>
+          <div className="flex justify-between items-center mt-2">
+          <div>${product.price}</div>
+            <span className="text-sm text-muted-foreground">In Stock: {product.stock}</span>
+          </div>
+        </CardHeader>
+      </Card>
+    </>
   )
 }
 
 ProductCard.Loader = () => {
   return (
-    <Card className="flex flex-col space-y-3">
-      <Skeleton className="rounded-none aspect-[4/3]" />
-      <CardContent className="space-y-2 pb-0">
-        <Skeleton className="h-4 w-x-65%]" />
-        <Skeleton className="h-3 w-[95%]" />
-        <Skeleton className="h-3 w-[80%]" />
-        <Skeleton className="h-3 w-[85%]" />
-      </CardContent>
-      <div className="space-y-2 px-6">
-        <Skeleton className="h-3 w-[100px] mt-3" />
-      </div>
-      <CardFooter className="flex justify-between mt-auto">
-        <Skeleton className="h-[36px] w-[103px] mt-[24px] ml-auto" />
-      </CardFooter>
+    <Card className="flex flex-col">
+      <Skeleton className="aspect-square m-6" />
+      <CardHeader className="pt-0 mt-auto">
+        <div className="grid grid-cols-3 gap-2">
+          <div className="flex flex-col space-y-2 col-span-2">
+            <Skeleton className="h-4 w-[65%]" />
+            <Skeleton className="h-3" />
+          </div>
+          <Skeleton className="h-[36px] w-[36px] my-auto ml-auto" />
+        </div>
+        <Skeleton className="h-4 w-[60px] mt-4" />
+      </CardHeader>
     </Card>
   )
 }
